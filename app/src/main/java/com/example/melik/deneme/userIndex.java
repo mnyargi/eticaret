80f6f45679f7eeddc03bc43494c9f8b62c428563
mnyargi <mnyargi@gmail.com>
2016-11-23 17:06:06 +0300

    Case 3: User/employeur page files

---
+++ app/src/main/java/com/example/melik/deneme/userIndex.java
@@ -0,0 +1,21 @@
+package com.example.melik.deneme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by melik on 20.11.2016.
+ */
+public class userIndex extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userindex);
+    }
+
+    public void myClickHandler(View target) {
+        // Do stuff
+    }
+}
